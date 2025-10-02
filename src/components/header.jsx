56ec83455e6f6df30e177cc7810c60b07c28df61
Alex Cortell <alex.cortell.ifp@gmail.com>
2025-10-02 09:05:15 +0200

    introduction files

---
+++ src/components/header.jsx
@@ -0,0 +1,15 @@
+
+const header = () => {
+  return (
+    <header>
+      {/* Left Section */}
+      <div>
+        <img className="md:w-16 w-12"
+        src="public\images\logo.png" alt="logo-img" />
+      </div>
+    
+    </header>
+  )
+}
+
+export default header
